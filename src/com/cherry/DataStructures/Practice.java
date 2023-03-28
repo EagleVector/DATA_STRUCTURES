63c371e75e5bca795941ae599c81076e22b101e2
EagleVector <kumarsubhambili3@gmail.com>
2023-03-28 16:51:46 +0530

    practice

---
+++ src/com/cherry/DataStructures/Practice.java
@@ -0,0 +1,50 @@
+package com.cherry.DataStructures;
+
+public class Practice {
+    public String name = "Cherry";
+    public String surname = "Billi";
+}
+class Prnt {
+
+    public static void main(String[] args) {
+//        int a = 10;
+//        int b = 15;
+//        double c = 12.904;
+//        double d = 32.1234;
+//        System.out.println(maxim(maxim(a, b), maxim(c, d)));
+//        System.out.println(a + " " + b);
+//        swappy(a, b);
+//        System.out.println(a + " " + b);
+        Practice pra = new Practice();
+        System.out.println(pra.name + " " + pra.surname);
+
+    }
+
+}
+
+//    static void swappy(int x, int y) {
+//        int temp = x;
+//        x = y;
+//        y = temp;
+//        System.out.println(x + " " + y);
+//    }
+//
+//    static int maxim(int x, int y) {
+//        if (x > y) {
+//            return x;
+//        }
+//        else {
+//            return y;
+//        }
+//    }
+//
+//    static double maxim(double x, double y) {
+//        if (x > y) {
+//            return x;
+//        }
+//        else {
+//            return y;
+//        }
+//    }
+//
+//}
