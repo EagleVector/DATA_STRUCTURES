cdac9b0434988a9ab241f879bd905c5497b07a1b
EagleVector <kumarsubhambili3@gmail.com>
2023-04-06 07:17:45 +0530

    Occurence

--- src/com/cherry/DataStructures/SlideWindow.java
+++ src/com/cherry/DataStructures/SlideWindow.java
@@ -16,61 +16,71 @@ public class SlideWindow {
 //        System.out.println(larcontssubarr(arr));
 //        int[] arr = {1, 2, 3, 4};
 //        printsubarrays(arr);
-        int[] arr1 = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-        int[] arr2 = {3, 4, 5, 6, 7};
-        int[] arr3 = {1, 2, 3, 4, 5};
-        int[] arr4 = {0, 1, 2, 3, 4};
-        int l1 = arr1.length;
-        int l2 = arr2.length;
-        int l3 = arr3.length;
-        int l4 = arr4.length;
+//        int[] arr1 = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
+//        int[] arr2 = {3, 4, 5, 6, 7};
+//        int[] arr3 = {1, 2, 3, 4, 5};
+//        int[] arr4 = {0, 1, 2, 3, 4};
+//        int l1 = arr1.length;
+//        int l2 = arr2.length;
+//        int l3 = arr3.length;
+//        int l4 = arr4.length;
+//
+//        int[] list = new int[l1 + l2 + l3 + l4];
+//        for (int i = 0; i < arr1.length; i++) {
+//            list[i] = arr1[i];
+//        }
+//        for (int i = 0; i < arr2.length; i++) {
+//            list[arr1.length + i] = arr2[i];
+//        }
+//        for (int i = 0; i < arr3.length; i++) {
+//            list[arr1.length + arr2.length + i] = arr3[i];
+//        }
+//        for (int i = 0; i < arr4.length; i++) {
+//            list[arr1.length + arr2.length + arr3.length + i] = arr4[i];
+//        }
+//        System.out.println(Arrays.toString(list));
+//
+//        int[] nw = new int[list.length];
+//
+//        for (int i = 0; i < list.length; i++) {
+//            nw[list[i]]++;
+//        }
+//
+//        int max = nw[0];
+//        int i = 0;
+//        for (; i < nw.length - 1; i++) {
+//
+//            if (nw[i + 1] > nw[i]) {
+//                max = nw[i + 1];
+//            }
+//        }
+//        System.out.println(Arrays.toString(nw));
+//        System.out.println(max);
+//
+//        for (int j = 0; j < nw.length; j++) {
+//            if (nw[j] == max) {
+//                System.out.println(j);
+//                break;
+//            }
+//        }
 
-        int[] list = new int[l1 + l2 + l3 + l4];
-        for (int i = 0; i < arr1.length; i++) {
-            list[i] = arr1[i];
-        }
-        for (int i = 0; i < arr2.length; i++) {
-            list[arr1.length + i] = arr2[i];
-        }
-        for (int i = 0; i < arr3.length; i++) {
-            list[arr1.length + arr2.length + i] = arr3[i];
-        }
-        for (int i = 0; i < arr4.length; i++) {
-            list[arr1.length + arr2.length + arr3.length + i] = arr4[i];
-        }
-        System.out.println(Arrays.toString(list));
-
-        int[] nw = new int[list.length];
-
-        for (int i = 0; i < list.length; i++) {
-            int count = 0;
-            for (int j = 0; j < list.length; j++) {
-
-                if (list[i] == list[j]) {
-                    count++;
-                }
-            }
-            nw[list[i]] = count;
-        }
-        System.out.println(Arrays.toString(nw));
-
-        int max = nw[0];
-
-        for (int i = 0; i < nw.length; i++) {
-            if (nw[i] >= max) {
-                max = nw[i];
-            }
-        }
-        ArrayList<Integer> listx = new ArrayList<>();
-
-        for (int i = 0; i < nw.length; i++) {
-            if (nw[i] == max) {
-                listx.add(i);
-            }
-        }
-        System.out.println(listx);
-        System.out.println(listx.get(0));
-        System.out.println(max);
+//        int max = nw[0];
+//
+//        for (int i = 0; i < nw.length; i++) {
+//            if (nw[i] >= max) {
+//                max = nw[i];
+//            }
+//        }
+//        ArrayList<Integer> listx = new ArrayList<>();
+//
+//        for (int i = 0; i < nw.length; i++) {
+//            if (nw[i] == max) {
+//                listx.add(i);
+//            }
+//        }
+//        System.out.println(listx);
+//        System.out.println(listx.get(0));
+//        System.out.println(max);
     }
 
 //    public static void printsubarrays(int[] arr) {
