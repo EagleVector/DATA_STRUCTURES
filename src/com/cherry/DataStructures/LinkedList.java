ffb332ff9b6ba4638f995d651a0ff196ff355d83
EagleVector <kumarsubhambili3@gmail.com>
2023-04-08 21:16:33 +0530

    Linked List

---
+++ src/com/cherry/DataStructures/LinkedList.java
@@ -0,0 +1,7 @@
+package com.cherry.DataStructures;
+
+public class LinkedList {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/cherry/DataStructures/LiveLinkedList.java
@@ -0,0 +1,20 @@
+package com.cherry.DataStructures;
+
+import java.util.LinkedList;
+
+public class LiveLinkedList {
+    public static void main(String[] args) {
+
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Sunny");
+        list.add("Bunny");
+        list.add(1, "Cherry");
+        System.out.println(list);
+        System.out.println(list.get(1));
+        list.add(1, "Derry");
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+    }
+}
+
