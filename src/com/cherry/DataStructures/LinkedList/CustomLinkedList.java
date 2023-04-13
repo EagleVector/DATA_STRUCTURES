595e89134524209b2ab5b6b7c376d93e1637d089
EagleVector <kumarsubhambili3@gmail.com>
2023-04-13 09:29:44 +0530

    Linked List reversal

--- src/com/cherry/DataStructures/LinkedList/CustomLinkedList.java
+++ src/com/cherry/DataStructures/LinkedList/CustomLinkedList.java
@@ -268,4 +268,41 @@ public class CustomLinkedList {
         list.reverseInplace();
         list.display();
     }
+//    public ListNode reverseBetween(ListNode head, int left, int right) {
+//        if (left == right) {
+//            return head;
+//        }
+////        Skip the first left n - 1 nodes.
+//        ListNode current = head;
+//        ListNode prev = null;
+//
+//        for (int i = 0; i < left - 1 && current != null; i++) {
+//            prev = current;
+//            current = current.next;
+//        }
+//
+//        ListNode last = prev;
+//        ListNode newEnd = current;
+//        ListNode next = current.next;
+//
+////        Reversing the linked list.
+//
+//        for (int i = 0; current != null && i < right - left + 1; i++) {
+//            current.next = prev;
+//            prev = current;
+//            current = next;
+//            if (next != null) {
+//                next = next.next;
+//            }
+//        }
+//        if (last != null) {
+//            last.next = prev;
+//        }
+//        else {
+//            head = prev;
+//        }
+//        newEnd.next = current;
+//        return head;
+//    }
+
 }
