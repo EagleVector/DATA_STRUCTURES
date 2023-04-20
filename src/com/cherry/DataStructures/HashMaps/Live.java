6e9087685175226625a1397812e43c0f2b79b89a
EagleVector <kumarsubhambili3@gmail.com>
2023-04-21 00:42:44 +0530

    Live HashMaps

--- src/com/cherry/DataStructures/HashMaps/Live.java
+++ src/com/cherry/DataStructures/HashMaps/Live.java
@@ -1,38 +1,75 @@
 package com.cherry.DataStructures.HashMaps;
 
-
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
 public class Live {
     public static void main(String[] args) {
-        //    Create a Hashmap
-        HashMap<String, Integer> languages = new HashMap<>();
+//        Create a Hashmap
+//        HashMap<String, Integer> languages = new HashMap<>();
 //        Add Elements in hashmap
-        languages.put("Java", 8);
-        languages.put("JS", 15);
-        languages.put("Python", 18);
-        languages.put("JS", 30);
-
-        System.out.println(languages);
-        int val = languages.get("JS");
-
-        System.out.println(val);
-
-        System.out.println(languages.keySet());
-        System.out.println(languages.values());
-        System.out.println(languages.entrySet());
-
+//        languages.put("Java", 8);
+//        languages.put("JS", 15);
+//        languages.put("Python", 18);
+//        languages.put("JS", 30);
+//
+//        System.out.println(languages);
+//        int val = languages.get("JS");
+//
+//        System.out.println(val);
+//
+//        System.out.println(languages.keySet());
+//        System.out.println(languages.values());
+//        System.out.println(languages.entrySet());
+//
 //        Replace method is used to change the value associated with a key in hashmap.
-        languages.replace("Python", 25);
-        System.out.println(languages);
+//        languages.replace("Python", 25);
+//        System.out.println(languages);
 //        Remove will remove the key in the hashmap.
-        languages.remove("Python");
-        System.out.println(languages);
+//        languages.remove("Python");
+//        System.out.println(languages);
 //        Check if the hashmap is empty.
-        System.out.println(languages.isEmpty());
+//        System.out.println(languages.isEmpty());
+//
+//        System.out.println(maxfreqChar("aabcddcbbbf"));
+//        int[] arr = {91, 74, 66, 48};
+//        int k = 12;
+//        System.out.println(pairDivisible(arr, k));
     }
 
-//    Add elements to Hashmaps.
+
+//    public static char maxfreqChar(String str) {
+//
+//        HashMap<Character, Integer> map = new HashMap<>();
+//
+//        for (int i = 0; i < str.length(); i++) {
+//            if (map.containsKey(str.charAt(i))) {
+//                int of = map.get(str.charAt(i));
+//                int nf = of + 1;
+//
+//                map.put(str.charAt(i), nf);
+//            }
+//            else {
+//                map.put(str.charAt(i), 1);
+//            }
+//        }
+//        System.out.println(map);
+//
+//        ArrayList<Character> list = new ArrayList<>(map.keySet());
+//
+//        char maxChar = ' ';
+//        int maxFreq = 0;
+//
+//        for (char ch : list) {
+//            if (map.get(ch) > maxFreq) {
+//                maxFreq = map.get(ch);
+//                maxChar = ch;
+//            }
+//        }
+//        return maxChar;
+//    }
+
 
 }
