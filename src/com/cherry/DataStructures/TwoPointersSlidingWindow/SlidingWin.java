71a8237d7e3c67b620bbffbf0d2ba24b0491e8b9
EagleVector <kumarsubhambili3@gmail.com>
2023-05-06 20:49:25 +0530

    Sliding Window Interview Problems

--- src/com/cherry/DataStructures/TwoPointersSlidingWindow/SlidingWin.java
+++ src/com/cherry/DataStructures/TwoPointersSlidingWindow/SlidingWin.java
@@ -1,16 +1,48 @@
 package com.cherry.DataStructures.TwoPointersSlidingWindow;
 
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class SlidingWin {
     public static void main(String[] args) {
 //        String str = "qrsvbspk";
 //        int ans = longestString(str);
 //        System.out.println(ans);
+//        String str = "AABABBA";
+//        int k = 2;
+//        int ans = characterReplace(str, k);
+//        System.out.println(ans);
+        String s1 = "ab";
+        String s2 = "eidbaooo";
+
+        boolean ans = permutationStr(s1, s2);
+        System.out.println(ans);
     }
+
+    private static boolean permutationStr(String s1, String s2) {
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        return true;
+
+    }
+
+//    private static int characterReplace(String str, int k) {
+//        HashMap<Character, Integer> map = new HashMap<>();
+//        int res = 0;
+//        int left = 0;
+//
+//        for (int right = 0; right < str.length(); right++) {
+//            map.put(str.charAt(right), map.getOrDefault(str.charAt(right), 0) + 1);
+//
+//            while (((right - left + 1) - Collections.max(map.values()) > k)) {
+//                map.put(str.charAt(left), map.get(str.charAt(left)) - 1);
+//                left++;
+//            }
+//            res = Math.max(right - left + 1, res);
+//        }
+//        return res;
+//    }
 
 //    private static int longestString(String str) {
 //        HashSet<Character> set = new HashSet<>();
