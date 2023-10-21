1d7486266c1f1882dc73ff94b5898958efed3d79
EagleVector <kumarsubhambili3@gmail.com>
2023-10-21 18:36:16 +0530

    Pre In Post Order Tree Traversals

--- src/com/cherry/DataStructures/Trees/BinaryTree.java
+++ src/com/cherry/DataStructures/Trees/BinaryTree.java
@@ -81,10 +81,58 @@ public class BinaryTree {
         prettyDisplay(node.left, level + 1);
     }
 
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.print(node.value + " ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.print(node.value + " ");
+        inOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    private void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value + " ");
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         BinaryTree tree = new BinaryTree();
         tree.populate(scanner);
         tree.prettyDisplay();
+        System.out.println();
+        tree.preOrder();
+        System.out.println();
+        tree.inOrder();
+        System.out.println();
+        tree.postOrder();
     }
 }
